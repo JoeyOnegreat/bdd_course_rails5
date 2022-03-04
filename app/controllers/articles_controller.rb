86dc01b38cfcaab61ca541634539e6bae7d88203
Joey Williamson <joey@onegreatstudio.com>
2022-03-04 10:14:20 -0500

    Implement Showing Article Details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,16 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
-  def new 
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
   end
-
+  
   def create
     @article = Article.new(article_params) 
     if @article.save
