162e6fd0bbb5db33bb13187b1180491fc990487e
Joey Williamson <joey@onegreatstudio.com>
2022-03-04 11:45:22 -0500

    complete article step 2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,28 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article 
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
   end
   
   def create
@@ -21,8 +35,25 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
-    
+
+  def destroy
+    if @article.destroy
+      flash[:success] = "Article has been deleted."
+      redirect_to articles_path 
+    end
+  end
+
+  protected
+  def resource_not_found
+    message = "The article you are looking for could not be found" 
+    flash[:alert] = message
+    redirect_to root_path
+  end
     private
+    def set_article
+      @article = Article.find(params[:id])
+      end
+
     def article_params
     params.require(:article).permit(:title, :body) 
     end
